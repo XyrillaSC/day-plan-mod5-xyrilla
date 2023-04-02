d66b2fdb04bc50b6bc1062371f42aed13fe2edbc
XyrillaSC <professionalstevenkramer@gmail.com>
2023-04-02 14:19:36 -0400

    added dayjs func

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -9,15 +6,32 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
 });
+var today = dayjs()
+$('#currentDay').text(`Today is ${today.format('dddd, MMMM D YYYY')}`)
+
+
+let hours = [$('#hour-9'), $('#hour-10'), $('#hour-11'), $('#hour-12'), $('#hour-13'), $('#hour-14'), $('#hour-15'), $('#hour-16'), $('#hour-17')]
+
+
+// console.log(hours)
+
+hours.forEach(function (hour) {
+  console.log(hour)
+  console.log(dayjs().hour())
+  if (hour.data('hour') < dayjs().hour()) {
+    hour.removeClass('present').addClass('past')
+  }
+  else if (hour.data('hour') === dayjs().hour()) {
+    hour.removeClass('future').addClass('present')
+  }
+  else if (hour.data('hour') > dayjs().hour()) {
+    hour.removeClass('past').addClass('future')
+  }
+})
